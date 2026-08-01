4a8c5507f1d755de9465945052e8603069c9afb2
SirLouen <sir.louen@gmail.com>
2026-08-01 23:11:34 +0200

    feat(editor): update dependencies and enhance styles for better layout

--- sdk/frontend/editor/styles.ts
+++ sdk/frontend/editor/styles.ts
@@ -6,8 +6,40 @@ import blockLibraryTheme from '@wordpress/block-library/build-style/theme.css?in
 const CANVAS_TYPOGRAPHY = `
 	body {
 		font-family: system-ui, sans-serif;
+		font-size: 16px;
 		margin: 0;
-		padding: 32px;
+		padding: 32px 0;
+	}
+	p {
+		font-size: inherit;
+		line-height: inherit;
+	}
+	ul,
+	ol {
+		margin: 0;
+		padding: 0 0 0 1.5em;
+	}
+	ul {
+		list-style-type: disc;
+	}
+	ol {
+		list-style-type: decimal;
+	}
+`
+
+const CANVAS_COLUMN = `
+	.wp-block {
+		max-width: 700px;
+		margin-left: auto;
+		margin-right: auto;
+	}
+	.wp-block[data-align="wide"],
+	.wp-block.alignwide {
+		max-width: 900px;
+	}
+	.wp-block[data-align="full"],
+	.wp-block.alignfull {
+		max-width: none;
 	}
 `
 
@@ -15,4 +47,5 @@ export const CANVAS_STYLES = [
 	{ css: blockLibraryStyle },
 	{ css: blockLibraryTheme },
 	{ css: CANVAS_TYPOGRAPHY },
+	{ css: CANVAS_COLUMN },
 ]
