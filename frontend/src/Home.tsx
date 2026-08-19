5c320c2ee7a3785dafe15915af3c7487129da1cc
SirLouen <sir.louen@gmail.com>
2026-08-20 01:12:52 +0200

    demo(i18n): take the drill string back out of the pipeline

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -12,7 +12,6 @@ export function Home() {
 	return (
 		<Page title={__('Home', 'gophenberg')}>
 			<Text>{__('Welcome to Gophenberg.', 'gophenberg')}</Text>
-			<Text>{__('This line proves the translation pipeline and is removed again.', 'gophenberg')}</Text>
 		</Page>
 	)
 }
