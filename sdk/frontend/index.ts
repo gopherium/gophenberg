b3eda218f68f9c16471fb50458404a1205e02180
SirLouen <sir.louen@gmail.com>
2026-08-31 17:36:04 +0200

    feat(sdk): offer the checkbox control beside the range control

--- sdk/frontend/index.ts
+++ sdk/frontend/index.ts
@@ -50,7 +50,7 @@ export {
 	chevronUp as upIcon,
 } from '@wordpress/icons'
 export { TextareaControl } from './TextareaControl'
-export { RangeControl } from '@wordpress/components'
+export { CheckboxControl, RangeControl } from '@wordpress/components'
 export {
 	ADMIN,
 	AUTHOR,
